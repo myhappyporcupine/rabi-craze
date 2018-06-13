90246e8ff56c5abe4580c6215aec3819f3900ea5
Ran Shoham <my.happy.porcupine@gmail.com>
2018-06-13 22:34:07 +0300

    Bug fix
    Fixed a buggy collision detection function.

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ const ctx = canvas.getContext('2d');
 
 // Collision
 function collision(rect1, rect2) {
-  return (((rect2.x >= rect1.x && rect2.x <= rect1.x + rect1.width) ||
-           (rect2.x + rect2.width >= rect1.x && rect2.x + rect2.width <= rect1.x + rect1.width)) &&
-          ((rect2.y >= rect1.y && rect2.y <= rect1.y + rect1.height) ||
-           (rect2.y + rect2.height >= rect1.y && rect2.y + rect2.height <= rect1.y + rect1.height)))
+	const x1 = rect1.x,
+	      y1 = rect1.y,
+	      w1 = rect1.width,
+	      h1 = rect1.height,
+	      x2 = rect2.x,
+	      y2 = rect2.y,
+	      w2 = rect2.width,
+	      h2 = rect2.height;
+
+	return ( x1 - w2 <= x2 && x2 <= x1 + w1 &&
+	         y1 - h2 <= y2 && y2 <= y1 + h1    )
 }
 
 // Input Handling
