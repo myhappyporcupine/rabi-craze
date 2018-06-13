17c42f2566aee12355c5ccdd51c367ec0411fd5f
Ran Shoham <my.happy.porcupine@gmail.com>
2018-06-13 22:41:49 +0300

    Format code
    I've replaced each tab with 2 spaces.

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ const ctx = canvas.getContext('2d');
 
 // Collision
 function collision(rect1, rect2) {
-	const x1 = rect1.x,
-	      y1 = rect1.y,
-	      w1 = rect1.width,
-	      h1 = rect1.height,
-	      x2 = rect2.x,
-	      y2 = rect2.y,
-	      w2 = rect2.width,
-	      h2 = rect2.height;
+  const x1 = rect1.x,
+        y1 = rect1.y,
+        w1 = rect1.width,
+        h1 = rect1.height,
+        x2 = rect2.x,
+        y2 = rect2.y,
+        w2 = rect2.width,
+        h2 = rect2.height;
 
-	return ( x1 - w2 <= x2 && x2 <= x1 + w1 &&
-	         y1 - h2 <= y2 && y2 <= y1 + h1    )
+  return ( x1 - w2 <= x2 && x2 <= x1 + w1 &&
+           y1 - h2 <= y2 && y2 <= y1 + h1    )
 }
 
 // Input Handling
