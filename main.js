70e3cd3a0a0917ffa7fb169aa592eb451777c351
Ran Shoham <my.happy.porcupine@gmail.com>
2018-06-15 22:21:51 +0300

    Add even more character control

--- main.js
+++ main.js
@@ -6,19 +6,35 @@ const rabi = new Rabi(400, 550, 50, ctx);
 const wife = new Wife(200, 550, 50, ctx);
 
 function handleInput() {
-  if (inputs.left)    rabi.xPos             -= rabi.xPosDelta;
-  if (inputs.up)    { rabi.leftArmRotation  += rabi.leftArmRotationDelta;
-                      rabi.rightArmRotation += rabi.rightArmRotationDelta; }
-  if (inputs.right)   rabi.xPos             += rabi.xPosDelta;
-  if (inputs.down)  { rabi.leftArmRotation  -= rabi.leftArmRotationDelta;
-                      rabi.rightArmRotation -= rabi.rightArmRotationDelta; }
+  if (inputs.left)       rabi.xPos             -= rabi.xPosDelta;
+  if (inputs.up)       { rabi.leftArmRotation  += rabi.leftArmRotationDelta;
+                         rabi.rightArmRotation += rabi.rightArmRotationDelta; }
+  if (inputs.right)      rabi.xPos             += rabi.xPosDelta;
+  if (inputs.down)     { rabi.leftArmRotation  -= rabi.leftArmRotationDelta;
+                         rabi.rightArmRotation -= rabi.rightArmRotationDelta; }
+	if (inputs.home)       rabi.headRotation     -= rabi.headRotationDelta;
+	if (inputs.pageup)     rabi.headRotation     += rabi.headRotationDelta;
+	if (inputs.end)        rabi.bodyRotation     -= rabi.bodyRotationDelta;
+	if (inputs.pagedown)   rabi.bodyRotation     += rabi.bodyRotationDelta;
+	if (inputs.enter)    { rabi.leftArmRotation   = 0;
+                         rabi.rightArmRotation  = 0;
+                         rabi.headRotation      = 0;
+												 rabi.bodyRotation      = 0;                          }
 
-  if (inputs.a)       wife.xPos             -= wife.xPosDelta;
-  if (inputs.w)     { wife.leftArmRotation  += wife.leftArmRotationDelta;
-                      wife.rightArmRotation += wife.rightArmRotationDelta; }
-  if (inputs.d)       wife.xPos             += wife.xPosDelta;
-  if (inputs.s)     { wife.leftArmRotation  -= wife.leftArmRotationDelta;
-                      wife.rightArmRotation -= wife.rightArmRotationDelta; }
+  if (inputs.a)          wife.xPos             -= wife.xPosDelta;
+  if (inputs.w)        { wife.leftArmRotation  += wife.leftArmRotationDelta;
+                         wife.rightArmRotation += wife.rightArmRotationDelta; }
+  if (inputs.d)          wife.xPos             += wife.xPosDelta;
+  if (inputs.s)        { wife.leftArmRotation  -= wife.leftArmRotationDelta;
+                         wife.rightArmRotation -= wife.rightArmRotationDelta; }
+	if (inputs.q)          wife.headRotation     -= wife.headRotationDelta;
+	if (inputs.e)          wife.headRotation     += wife.headRotationDelta;
+	if (inputs.z)          wife.bodyRotation     -= wife.bodyRotationDelta;
+	if (inputs.c)          wife.bodyRotation     += wife.bodyRotationDelta;
+	if (inputs.r)        { wife.leftArmRotation   = 0;
+                         wife.rightArmRotation  = 0;
+                         wife.headRotation      = 0;
+												 wife.bodyRotation      = 0;                          }
 }
 
 (function frame() {
--- util.js
+++ util.js
@@ -1,34 +1,64 @@
 const inputs = {
-  left  : false,
-  up    : false,
-  right : false,
-  down  : false,
-  a     : false,
-  w     : false,
-  d     : false,
-  s     : false
+  left     : false,
+  up       : false,
+  right    : false,
+  down     : false,
+	home     : false,
+	pageup   : false,
+	end      : false,
+	pagedown : false,
+	enter		 : false,
+  a        : false,
+  w        : false,
+  d        : false,
+  s        : false,
+	q        : false,
+	e        : false,
+	z        : false,
+  c        : false,
+	r				 : false
 }
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowLeft')  inputs.left  = true;
-  if (event.key === 'ArrowUp')    inputs.up    = true;
-  if (event.key === 'ArrowRight') inputs.right = true;
-  if (event.key === 'ArrowDown')  inputs.down  = true;
-  if (event.key === 'a')          inputs.a     = true;
-  if (event.key === 'w')          inputs.w     = true;
-  if (event.key === 'd')          inputs.d     = true;
-  if (event.key === 's')          inputs.s     = true;
+  if (event.key === 'ArrowLeft')  inputs.left     = true;
+  if (event.key === 'ArrowUp')    inputs.up       = true;
+  if (event.key === 'ArrowRight') inputs.right    = true;
+  if (event.key === 'ArrowDown')  inputs.down     = true;
+  if (event.key === 'Home')       inputs.home     = true;
+  if (event.key === 'PageUp')     inputs.pageup   = true;
+  if (event.key === 'End')        inputs.end      = true;
+  if (event.key === 'PageDown')   inputs.pagedown = true;
+  if (event.key === 'Enter')      inputs.enter    = true;
+  if (event.key === 'a')          inputs.a        = true;
+  if (event.key === 'w')          inputs.w        = true;
+  if (event.key === 'd')          inputs.d        = true;
+  if (event.key === 's')          inputs.s        = true;
+  if (event.key === 'q')          inputs.q        = true;
+  if (event.key === 'e')          inputs.e        = true;
+  if (event.key === 'z')          inputs.z        = true;
+  if (event.key === 'c')          inputs.c        = true;
+  if (event.key === 'r')          inputs.r        = true;
 });
 
 document.addEventListener('keyup', (event) => {
-  if (event.key === 'ArrowLeft')  inputs.left  = false;
-  if (event.key === 'ArrowUp')    inputs.up    = false;
-  if (event.key === 'ArrowRight') inputs.right = false;
-  if (event.key === 'ArrowDown')  inputs.down  = false;
-  if (event.key === 'a')          inputs.a     = false;
-  if (event.key === 'w')          inputs.w     = false;
-  if (event.key === 'd')          inputs.d     = false;
-  if (event.key === 's')          inputs.s     = false;
+  if (event.key === 'ArrowLeft')  inputs.left     = false;
+  if (event.key === 'ArrowUp')    inputs.up       = false;
+  if (event.key === 'ArrowRight') inputs.right    = false;
+  if (event.key === 'ArrowDown')  inputs.down     = false;
+  if (event.key === 'Home')       inputs.home     = false;
+  if (event.key === 'PageUp')     inputs.pageup   = false;
+  if (event.key === 'End')        inputs.end      = false;
+  if (event.key === 'PageDown')   inputs.pagedown = false;
+  if (event.key === 'Enter')      inputs.enter    = false;
+  if (event.key === 'a')          inputs.a        = false;
+  if (event.key === 'w')          inputs.w        = false;
+  if (event.key === 'd')          inputs.d        = false;
+  if (event.key === 's')          inputs.s        = false;
+  if (event.key === 'q')          inputs.q        = false;
+  if (event.key === 'e')          inputs.e        = false;
+  if (event.key === 'z')          inputs.z        = false;
+  if (event.key === 'c')          inputs.c        = false;
+  if (event.key === 'r')          inputs.r        = false;
 });
 
 function collision(rect1, rect2) {
